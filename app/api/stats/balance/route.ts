48895fc446c87e82b0b7dfcf498b0ce66f1301e6
Alberto Sierra <Sierra.Alberto@iCloud.com>
2024-05-12 23:39:14 -0700

    stats and history graph are working

--- app/api/stats/balance/route.ts
+++ app/api/stats/balance/route.ts
@@ -8,22 +8,27 @@ export async function GET(request: Request) {
   if (!user) {
     redirect("/sign-in");
   }
+
   const { searchParams } = new URL(request.url);
   const from = searchParams.get("from");
   const to = searchParams.get("to");
+
   const queryParams = OverviewQuerySchema.safeParse({ from, to });
   if (!queryParams.success) {
     return Response.json(queryParams.error.message, {
       status: 400,
     });
   }
+
   const stats = await getBalanceStats(
     user.id,
     queryParams.data.from,
     queryParams.data.to
   );
+
   return Response.json(stats);
 }
+
 export type GetBalanceStatsResponseType = Awaited<
   ReturnType<typeof getBalanceStats>
 >;
@@ -42,6 +47,7 @@ async function getBalanceStats(userId: string, from: Date, to: Date) {
       amount: true,
     },
   });
+
   return {
     expense: totals.find((t) => t.type === "expense")?._sum.amount || 0,
     income: totals.find((t) => t.type === "income")?._sum.amount || 0,
--- app/api/stats/categories/route.ts
+++ app/api/stats/categories/route.ts
@@ -1,21 +1,24 @@
 import prisma from "@/lib/prisma";
 import { OverviewQuerySchema } from "@/schema/overview";
 import { currentUser } from "@clerk/nextjs/server";
+import { Return } from "@prisma/client/runtime/library";
 import { redirect } from "next/navigation";
-import { tuple } from "zod";
 
 export async function GET(request: Request) {
   const user = await currentUser();
   if (!user) {
     redirect("/sign-in");
   }
+
   const { searchParams } = new URL(request.url);
   const from = searchParams.get("from");
   const to = searchParams.get("to");
+
   const queryParams = OverviewQuerySchema.safeParse({ from, to });
   if (!queryParams.success) {
     throw new Error(queryParams.error.message);
   }
+
   const stats = await getCategoriesStats(
     user.id,
     queryParams.data.from,
@@ -47,4 +50,6 @@ async function getCategoriesStats(userId: string, from: Date, to: Date) {
       },
     },
   });
+
+  return stats;
 }
